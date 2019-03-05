5a86a196b15fe826774f779f791ec32875c5b8bb
Yaroslav <yorimark@abv.bg>
2019-03-05 10:51:11 +0200

    dsadas

--- main.tf
+++ main.tf
@@ -4,10 +4,12 @@ resource "null_resource" "helloWorld" {
   }
 }
 
+
 #provider "uptimerobot" {
-# api_key = "dasdasdas"
+#  "plugin_url" = "https://github.com/yaroslav-007/13848/raw/master/tets/terraform-provider-uptimerobot_v0.2.1"
 #}
 
-provider "uptimerobot" {
-  "plugin_url" = "https://iaroslav.eu/s/qmwzoXbLFYGoeeF/download"
+module "consul" {
+  source  = "https://github.com/yaroslav-007/13848/raw/master/tets/"
+  servers = 3
 }

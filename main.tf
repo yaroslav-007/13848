3b045e28cb9a22d5de94baa568eef196fc6c93cb
Yaroslav <yorimark@abv.bg>
2019-03-05 08:45:27 +0200

    upload main

--- main.tf
+++ main.tf
@@ -3,3 +3,12 @@ resource "null_resource" "helloWorld" {
     command = "echo hello world"
   }
 }
+
+
+#provider "uptimerobot" {
+# api_key = "dasdasdas"
+#}
+
+provider "uptimerobot" {
+     "plugin_url" = "https://github.com/yaroslav-007/13848/blob/master/terraform-provider-uptimerobot"
+}

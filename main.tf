3d527a5c79178066d75ca8149e1ec3cc3a07a3b0
Yaroslav <yorimark@abv.bg>
2019-03-05 11:21:41 +0200

    final

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
-resource "null_resource" "helloWorld" {
-  provisioner "local-exec" {
-    command = "echo hello world"
-  }
-}
-
-
-#provider "uptimerobot" {
-#  "plugin_url" = "https://github.com/yaroslav-007/13848/raw/master/tets/terraform-provider-uptimerobot_v0.2.1"
+#resource "null_resource" "helloWorld" {
+ # provisioner "local-exec" {
+#    command = "echo hello world"
+#  }
 #}
 
-module "consul" {
-  source  = "https://github.com/yaroslav-007/13848/raw/master/tets/"
-  servers = 3
+
+provider "uptimerobot" {
+  plugin_url = "https://github.com/yaroslav-007/13848/blob/master/tets/terraform-provider-uptimerobot_v0.2.1"
+ api_key = "das"
 }
+
--- tets/main.tf
+++
@@ -1,6 +0,0 @@
-resource "null_resource" "helloWorld" {
-  provisioner "local-exec" {
-    command = "echo hello world2"
-
-provider "uptimerobot" {
-}
